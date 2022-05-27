980249c2f7c495418469b1e59f088649bd7989bd
Loly Maluf <lolymaluf@gmail.com>
2022-05-27 18:12:46 -0300

    adiciona req-7

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { getProductFromId } from '../services/api';
 
 export default class ProductDetails extends React.Component {
+  state = {
+    product: {},
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  getProduct = async () => {
+    const { match: { params: { id } } } = this.props;
+    const product = await getProductFromId(id);
+    this.setState({ product });
+  }
+
   render() {
+  /*     console.log('log do state', this.state.product); */
+    const { product } = this.state;
     return (
       <div data-testid="product">
         <h1>Product Details</h1>
-        <p>{this.props.match.params.id}</p>
+        <p data-testid="product-detail-name">{ product.title }</p>
+        <p>{ product.price }</p>
+        <img alt="imagem do produto" src={ product.thumbnail } />
       </div>
     );
   }
 }
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
