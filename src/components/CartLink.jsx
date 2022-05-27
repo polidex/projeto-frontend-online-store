715992ff164f8239fad69d3308df6553685d3fa2
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-27 19:43:25 -0300

    add comments to CartLink

--- src/components/CartLink.jsx
+++ src/components/CartLink.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class CartLink extends React.Component {
+  // Quero colocar uma div para contar a quantidade de itens no carrinho (bolinha vermelha com valor encima do elemento)
   render() {
     return (
       <Link id="cart_link" data-testid="shopping-cart-button" to="/cart">
