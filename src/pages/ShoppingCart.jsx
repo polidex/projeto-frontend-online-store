07cb573c71025ff2ecc7a6e376e3c7898918504c
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-27 22:12:03 -0300

    adicionado funcionalidade listar items do carrinho de compras no ShoppingCart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
+import { getItems } from '../services/saveItems';
 
 export default class ShoppingCart extends React.Component {
   state = {
     cartItems: [],
   }
 
+  componentDidMount() {
+    this.getCartItems();
+  }
+
+  getCartItems = () => {
+    this.setState({ cartItems: getItems() });
+  }
+
   render() {
     const { cartItems } = this.state;
     return (
-      cartItems.length === 0
-      && <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+      <div>
+        {cartItems.length === 0
+        && <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>}
+        {cartItems.length > 0
+        && cartItems.map((cartItem) => (
+          <ProductCard
+            key={ cartItem.id }
+            objProduct={ cartItem }
+          />))}
+      </div>
     );
   }
 }
